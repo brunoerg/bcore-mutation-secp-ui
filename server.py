31d4fffac539f7b92bb2a6f0e4b301cd384488b2
Bruno Garcia <brunoely.gc@gmail.com>
2026-03-11 14:42:47 -0300

    add new dashboard

---
+++ server.py
@@ -0,0 +1,10 @@
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+
+class CORSRequestHandler(SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header('Content-Type', 'text/html; charset=utf-8')
+        super().end_headers()
+
+httpd = HTTPServer(('localhost', 8000), CORSRequestHandler)
+print("Serving at port 8000")
+httpd.serve_forever()
